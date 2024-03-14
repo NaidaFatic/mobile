32cf63504581352e88384b5a728f54d697286efd
NaidaFatic <naidafatic@gmail.com>
2024-03-14 10:16:26 +0100

    lab3 compose intro

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -3,9 +3,15 @@ package com.example.myapplication.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val Blue80 = Color(0xFF00BCD4)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+val BlueGrey80 = Color(0xFF369CEC)
 val Pink80 = Color(0xFFEFB8C8)
+val Opal80 = Color(0xFF5AB4FC)
 
 val Purple40 = Color(0xFF6650a4)
+val Blue40 = Color(0xFF1866A3)
 val PurpleGrey40 = Color(0xFF625b71)
+val BlueGrey40 = Color(0xFF6675CA)
 val Pink40 = Color(0xFF7D5260)
+val Opal40 = Color(0xFF576AD1)
--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue80,
+    secondary = BlueGrey80,
+    tertiary = Opal80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue40,
+    secondary = BlueGrey40,
+    tertiary = Opal40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
