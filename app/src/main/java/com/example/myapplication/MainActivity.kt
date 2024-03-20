7fa11c881ba1f06cd2c9829a7e74869dfcf08ed2
NaidaFatic <naidafatic@gmail.com>
2024-03-20 12:33:45 +0100

    lab4 click button and push notification

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.screen.LoginScreen
+import com.example.myapplication.ui.screen.RegistrationScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    //LoginScreen()
+                    RegistrationScreen(this)
                 }
             }
         }
